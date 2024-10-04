a508851f5eca3d0d5a02fcf8a945c4f58e943df0
Hikoo <shoka060131@gmail.com>
2024-10-04 14:43:17 +0800

    1004

---
+++ test_10_04/test1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define NUM 100000000
+
+int main()
+{
+    int i = 1;
+     long long int sum = 0;
+
+    for (i = 1; i <= NUM; i++)
+    {
+        sum += i;
+    }
+
+    printf("sum = %lld\n", sum);
+
+    return 0;
+}
